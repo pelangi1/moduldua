db4b560ea78cda604d6f72ce3f7c309f9257cd27
keen eyes <56186677+pelangi1@users.noreply.github.com>
2021-03-23 09:36:53 +0700

    Update sssbekup.java

--- sssbekup.java
+++ sssbekup.java
@@ -2,44 +2,39 @@ package com.modul.dua;
 
 
 
-import java.util.*;
+import java.util.Scanner;
 
-public class makanan {
+public class lingkaran {
     public static void main(String[] args){
-        double phi = 3.14;
-        double r, keliling, Luas;
-        int pilih;
-
+	int pilih;
+        double phi = 3.14, r, keliling, Luas;
+      
         @SuppressWarnings("resource")
-		Scanner x = new Scanner(System.in);
-        System.out.println("KALKULATOR LINGKARAN");
+	Scanner pb = new Scanner(System.in);
+        System.out.println("Luas lingkaran");
         System.out.println("1. Menghitung Luas");
         System.out.println("2. Menghitung Keliling");
         System.out.print("Pilih Proses Hitungan : ");
-        pilih = x.nextInt();
+        pilih = pb.nextInt();
 
         switch (pilih){
             case 1:
                 System.out.println("\nMenghitung Luas");
                 System.out.print("Masukkan jari - jari(cm) :");
-                r = x.nextInt();
+                r = pb.nextInt();
                 Luas = phi*r*r;
-
-                System.out.println("Hasil : "+Luas+ " cm");
+                System.out.println("Hasil : "+Luas);
                 break;
-
-
             case 2:
                 System.out.println("\nMenghitung Keliling");
                 System.out.print("Masukkan jari - jari(cm) :");
-                r = x.nextInt();
+                r = pb.nextInt();
                 keliling = 2*phi*r;
-
-                System.out.println("Hasil : "+keliling+" cm");
+                System.out.println("Hasil : "+keliling);
                 break;
 
             default:
-                System.out.println("Proses hitung tidak ditemukan, program dihentikan.");
+                System.out.println("inputan salah");
         }
     }
 }
